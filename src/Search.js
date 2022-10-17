5015ec3042385d40e9cbb07a6496e635a98bc297
Bianca Slavov <bianca_slavov@hotmail.com>
2022-10-17 17:54:38 +0200

    Added the first functionality of the search field - using functions & a state

--- src/Search.js
+++ src/Search.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Search() {
+    let [keyword, setKeyword] = useState("");
+
+    function searchRecipe(event) {
+        event.preventDefault();
+        alert(`Searching for ${keyword}`);
+    }
+
+    function handleKeywordChange(event) {
+        setKeyword(event.target.value);
+    }
+
     return (
         <div className="Search">
-            Hi from Search
+            <form onSubmit={searchRecipe}>
+                <input type="search" onChange={handleKeywordChange} />
+            </form>
         </div>
     )
 }
