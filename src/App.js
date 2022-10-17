b82a1c6910db106bcdd94ac23e0c58ba8c6625e6
Bianca Slavov <bianca_slavov@hotmail.com>
2022-10-17 17:32:27 +0200

    Added a font, the footer and the Search component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import "./App.css";
+import Search from './Search'
+
 function App() {
   return (
     <div className="App">
-     <h1>Hello</h1>
+      <div className="container">
+      <header className="App-header">
+        Let's get cooking
+      </header>
+
+      <main>
+        <Search />
+      </main>
+
+      <footer className="App-footer">
+          {" "}
+          <a
+            href="https://github.com/Bianca-Slavov/dictionary-project-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+          by{" "}
+          <a
+            href="https://www.linkedin.com/in/biancaslavov/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Bianca Slavov
+          </a>
+        </footer>
+
+      </div>
     </div>
   );
 }
