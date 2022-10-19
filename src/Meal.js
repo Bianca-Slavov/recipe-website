1ee56d9383d700c8266b17896aef0aa46d507852
Bianca Slavov <bianca_slavov@hotmail.com>
2022-10-19 16:35:00 +0200

    Deleted some styling to make it work clearer + added new minor styling

--- src/Meal.js
+++ src/Meal.js
@@ -1,12 +1,14 @@
 import React from "react";
+import './Meal.css';
 
 export default function Meal(props) {
     console.log(props);
 return(
     <div className="Meal">
-    <h3>{props.meal.strMeal}</h3>
-    <p>{props.meal.strInstructions}</p>
-
+        <div className="card">
+    <h5>{props.meal.strMeal}</h5>
+    <img src={props.meal.strMealThumb} alt="meal"/>
+    </div>
 </div>
 )
 }
